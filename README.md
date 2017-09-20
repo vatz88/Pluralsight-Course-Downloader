117592ae3e9341318b157eb3a2b3a6cbdd4bc343
Vatsal Joshi <vatz88@gmail.com>
2017-09-20 23:35:21 +0530

    :sparkles: Downloaded video organised in folder

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 ## Installation
 
-Clone the repository to your system or download it as [.zip](/zipball/master) or [.tar.gz](/tarball/master).
+Clone the repository to your system or download it as [.zip](https://github.com/vatz88/Pluralsight-Course-Downloader/zipball/master) or [.tar.gz](https://github.com/vatz88/Pluralsight-Course-Downloader/tarball/master).
 
 Follow this [guide](https://developer.chrome.com/extensions/getstarted#unpacked) to load extension in developer mode in Chrome browser.
 
 ## Usage
 
-Hit the `s` key while watching the video on pluralsight and it'll be downloaded!
+Hit the `s` key while watching the video on pluralsight and it'll be downloaded and organized in folder with appropriate file name!
--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 $(function () {
-  console.log("Hey! script working!");
+
   $(document).keydown(function (e) {
+
     if (e.key === "s") {
+
       var link = $("#vjs_video_3_html5_api").attr("src");
-      var fname = $("li.selected").eq(1).index() + 1 + " - " + $("#module-clip-title").text().split(":").pop().trim() + "." + link.split("?")[0].split(".").pop();
+
+      var coursename = $("#course-title-link").text();
+
+      var folder = $("li.selected").parents("ul").prev("header").children("div");
+      folder = folder.eq(0).text() + " - " + folder.eq(1).find("h2").text();
+
+      var filename = $("li.selected").eq(1).index() + 1 + " - " + $("#module-clip-title").text().split(":").pop().trim() + "." + link.split("?")[0].split(".").pop();
+
       chrome.runtime.sendMessage({
         greeting: "download",
         link: link,
-        filename: fname
+        filename: "Pluralsight/" + coursename + "/" + folder + "/" + filename
       }, function (response) {
         console.log(response.farewell);
       });
+
     }
+
   });
+
 });
