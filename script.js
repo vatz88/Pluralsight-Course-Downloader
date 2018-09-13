f7be141586f1d53c86c3b409bbb8b9d16d5c661a
Vatsal Joshi <vatz88@gmail.com>
2018-09-13 23:53:49 +0530

    #6 code review, downloadAllVideosTimeout set to 30sec

--- script.js
+++ script.js
@@ -1,64 +1,59 @@
 var count = 0;
 
-Number.prototype.myPadding = function() {
-
+Number.prototype.myPadding = function () {
   var number = this.valueOf();
-  var length =2;
+  var length = 2;
   var str = '' + number;
- while (str.length < length) {
-     str = '0' + str;
- } 
- return str;
+  while (str.length < length) {
+    str = '0' + str;
+  }
+  return str;
 };
 
-var pauseVideo = function pauseVideo() {
+function pauseVideo() {
   if ($('#play-control').length === 1) {
     $('#play-control').click();
   }
 }
 
-var getCourseName = function(){  
+function getCourseName() {
   var courseName = $('#course-title-link').text();
   courseName = courseName.replace(/[\/:?><]/g, '');
   return courseName;
 }
 
-var getSectionDom = function(){
-  
+function getSectionDom() {
   var folderDom = $('li.selected')
     .parents('ul')
     .prev('header')
     .children('div')
     .eq(1);
-
-    return folderDom;
+  return folderDom;
 }
 
-var getSaveFilePath = function(){
-  
+function getSaveFilePath() {
   var link = $('#vjs_video_3_html5_api').attr('src');
   var courseName = getCourseName();
   // console.log(courseName);
 
   var folderDom = getSectionDom();
-
   var sectionName = folderDom.find('h2').text();
   var sectionIndex = (folderDom.parents('section.module.open').eq(0).index() + 1).myPadding();
-  var saveFolder =  sectionIndex + ' - ' +  sectionName;
+  var saveFolder = sectionIndex + ' - ' + sectionName;
   saveFolder = saveFolder.replace(/[\/:?><]/g, '');
   // console.log(saveFolder);
 
   var rawFileName = $('#module-clip-title').text().split(' : ').pop().trim();
-  var fileIndex =($('li.selected').eq(1).index() + 1).myPadding();
-  var saveFileName = fileIndex + ' - ' + rawFileName + '.' +  link.split('?')[0].split('.').pop();
+  var fileIndex = ($('li.selected').eq(1).index() + 1).myPadding();
+  var saveFileName = fileIndex + ' - ' + rawFileName + '.' + link.split('?')[0].split('.').pop();
   saveFileName = saveFileName.replace(/[\/:?><]/g, '');
   // console.log(saveFileName);
 
-  console.log('processing => ' + courseName + ' ' + sectionIndex + '-' + fileIndex);
+  console.log('processing => ' + courseName + ' ' + sectionIndex + ' - ' + fileIndex);
   return 'Pluralsight/' + courseName + '/' + saveFolder + '/' + saveFileName;
 }
 
-var downloadCurrentVideo = function() {
+function downloadCurrentVideo() {
   var link = $('#vjs_video_3_html5_api').attr('src');
   console.log('downloadCurrentVideo: ' + link);
 
@@ -75,20 +70,18 @@ var downloadCurrentVideo = function() {
   );
 }
 
-var downloadAllVideos = function() {
-
-  var folderDom = getSectionDom();
+function downloadAllVideos() {
   var link = $('#vjs_video_3_html5_api').attr('src');
-  var sectionName = folderDom.find('h2').text();
-  var finalFolderName = $('section:last').find('h2').text();
-  var downloadAllVideosTimeout = 120000;
-  var pauseVideoTimeout = 8000;
-
-  var rawFileName = $('#module-clip-title').text().split(' : ').pop().trim();
-  var finalFileName = $('section:last').find('li:last').find('h3').text();
-
   var saveFilePath = getSaveFilePath();
   console.log('chrome download => ' + saveFilePath);
+
+  var downloadAllVideosTimeout = 30000;
+  var pauseVideoTimeout = 8000;
+  var folderDom = getSectionDom();
+  var sectionName = folderDom.find('h2').text();
+  var finalFolderName = $('section:last').find('h2').text();
+  var rawFileName = $('#module-clip-title').text().split(' : ').pop().trim();
+  var finalFileName = $('section:last').find('li:last').find('h3').text();
 
   chrome.runtime.sendMessage({
       action: 'download',
@@ -97,21 +90,18 @@ var downloadAllVideos = function() {
     },
     function (response) {
       console.log('response => ' + response.actionStatus);
-      if (sectionName == finalFolderName && rawFileName == finalFileName) { 
+      if (sectionName == finalFolderName && rawFileName == finalFileName) {
         alert("Full Course Downloaded!");
-      }
-      else {         
+      } else {
         $('#next-control').click();
         setTimeout(pauseVideo, pauseVideoTimeout);
-        setTimeout(downloadAllVideos, downloadAllVideosTimeout); 
+        setTimeout(downloadAllVideos, downloadAllVideosTimeout);
       }
     }
   );
 }
 
-
 $(function () {
-
   $(document).keypress(function (e) {
     if (e.which === 115 || e.which === 83) {
       // keypress `s`
